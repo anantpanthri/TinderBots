77bb028db47b632126dd0ac2000ee21a11971ae5
anantpanthri <anant.shripanthri@gmail.com>
2020-02-16 14:34:57 -0500

    Initial commit

---
+++ secrets.py
@@ -0,0 +1,2 @@
+username='facebook username'
+password='facebook password'
---
+++ tinder2.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from time import sleep
+
+from secrets import username, password
+
+class TinderBot():
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+    def login(self):
+        self.driver.get('https://tinder.com')
+
+        sleep(2)
+
+#
+#
